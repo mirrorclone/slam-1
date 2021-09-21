774e4623c0a0076fa34fe0734f038f799ddfe782
shivayn7 <80518569+shivayn7@users.noreply.github.com>
2021-09-21 08:11:21 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -111,7 +111,7 @@ def get_readable_message():
         for download in list(download_dict.values()):
             INDEX += 1
             if INDEX > COUNT:
-                msg += f"\n<b>🚦 Status:</b><i>{download.status()}</i> {download.progress()}"
+                msg += f"\n<b>🚦 Status:</b> <i>{download.status()}</i> {download.progress()}"
                 msg += f"\n<b>📁 ғɪʟᴇɴᴀᴍᴇ:</b> <code>{download.name()}</code>"       
                 if download.status() not in [
                     MirrorStatus.STATUS_ARCHIVING,
