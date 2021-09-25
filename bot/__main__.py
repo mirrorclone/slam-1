4d7be59190df265d670b8e9300d857b9a166d07c
shivayn7 <80518569+shivayn7@users.noreply.github.com>
2021-09-25 07:31:19 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -31,15 +31,12 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>Bot Uptime:</b> <code>{currentTime}</code>\n' \
-            f'<b>Total Disk Space:</b> <code>{total}</code>\n' \
-            f'<b>Used:</b> <code>{used}</code> ' \
-            f'<b>Free:</b> <code>{free}</code>\n\n' \
-            f'<b>Upload:</b> <code>{sent}</code>\n' \
-            f'<b>Download:</b> <code>{recv}</code>\n\n' \
-            f'<b>CPU:</b> <code>{cpuUsage}%</code> ' \
-            f'<b>RAM:</b> <code>{memory}%</code> ' \
-            f'<b>DISK:</b> <code>{disk}%</code>'
+    stats = f'<b>⏰ ʙᴏᴛ ᴜᴘᴛɪᴍᴇ:</b> <code>{currentTime}</code>\n' \
+            f'<b>💾 ᴛᴏᴛᴀʟ ᴅɪsᴋ sᴘᴀᴄᴇ:</b> <code>{total}</code>\n' \
+            f'<b>⛔️ ᴜsᴇᴅ:</b> <code>{used}</code> ' \
+            f'<b>✅ ғʀᴇᴇ:</b> <code>{free}</code>\n\n' \
+            f'<b>🔺 ᴜᴘʟᴏᴀᴅ:</b> <code>{sent}</code>\n' \
+            f'<b>🔻 ᴅᴏᴡɴʟᴏᴀᴅ:</b> <code>{recv}</code>'         
     sendMessage(stats, context.bot, update)
 
 
